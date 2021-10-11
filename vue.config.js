75d6a8943f2a4e06f76da04feadf4218dc2bc1a9
刘平 <g1792202861@gmail.com>
2021-10-11 14:37:34 +0800

    fix:修改vue.config配置

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // 基本路径
-  pulicPath: process.env.NOOD.ENV === 'production' ? '' : '/',
+  // pulicPath: process.env.NOOD_ENV === 'production' ? '' : '/',
   // 构建时的输出目录
-  outputDir: 'dist',
+  // outputDir: 'dist',
   // 防止静态资源的目录
-  assetDir: 'static',
+  // assetDir: 'static',
   // html的输出路径
-  indexPath: 'index.html',
+  // indexPath: 'index.html',
   // 文件名哈希
-  filenameHashing: true,
+  // filenameHashing: true,
   // 在保存时用'eslint-loader'检查
   lintOnSave: false,
 }
